572aa32e38c70423767b354c6d657c74a56e6785
joseCnarciso <josecdia@hotmail.com>
2023-12-11 23:28:14 -0300

    Cadastro de usuario

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Traits\HttpResponses;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserController extends Controller
+{
+    use HttpResponses;
+
+    public function store(Request $request){
+
+    try {
+
+        $data = $request->all();
+
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email'=>'email|required|max:255',
+            'password'=>'string|required|min:6|max:32',
+            'plan_id'=>'required|integer',
+            'cpf'=>'string|required|min:14|max:14',
+            'date_birth'=>'date|required'
+        ]);
+
+        if (User::where('email', $data['email'])->exists()) {
+            return $this->error('Email já cadastrado', Response::HTTP_CONFLICT);
+        }
+        if (User::where('cpf', $data['cpf'])->exists()) {
+            return $this->error('cpf já cadastrado', Response::HTTP_CONFLICT);
+        }
+
+        $user = User::create($data);
+
+        return $user;
+
+
+    } catch (\Exception $exception) {
+        return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
+    }
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,11 @@
 <?php
 
-
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
     // rotas privadas
 });
 
+Route::post('users', [UserController::class, 'store']);
 // rota pública
