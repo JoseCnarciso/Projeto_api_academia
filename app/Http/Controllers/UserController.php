b4237be2c34ca6aee87dc909125be2334cf9c3a3
joseCnarciso <josecdia@hotmail.com>
2023-12-13 22:42:08 -0300

    refazendo

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,45 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\SendWelcomeUser;
 use App\Models\User;
 use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
     use HttpResponses;
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
 
-    try {
+        try {
 
-        $data = $request->all();
+            $data = $request->all();
 
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'email'=>'email|required|max:255',
-            'password'=>'string|required|min:6|max:32',
-            'plan_id'=>'required|integer',
-            'cpf'=>'string|required|min:14|max:14',
-            'date_birth'=>'date|required'
-        ]);
+            $request->validate([
+                'name' => 'required|string|max:255',
+                'email' => 'email|required|max:255',
+                'password' => 'string|required|min:6|max:32',
+                'plan_id' => 'required|integer',
+                'cpf' => 'string|required|min:14|max:14',
+                'date_birth' => 'date|required'
+            ]);
 
-        if (User::where('email', $data['email'])->exists()) {
-            return $this->error('Email já cadastrado', Response::HTTP_CONFLICT);
+            if (User::where('email', $data['email'])->exists()) {
+                return $this->error('Email já cadastrado', Response::HTTP_CONFLICT);
+            }
+            if (User::where('cpf', $data['cpf'])->exists()) {
+                return $this->error('cpf já cadastrado', Response::HTTP_CONFLICT);
+            }
+
+            $user = User::create($data);
+
+            if (!empty($user->user_id)) {
+                $user = User::find($user->user_id);
+
+                Mail::to('josecdia@hotmail.com', 'José bastiao')
+                    ->send(new SendWelcomeUser());
+            }
+
+            return $user;
+        } catch (\Exception $exception) {
+            return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
         }
-        if (User::where('cpf', $data['cpf'])->exists()) {
-            return $this->error('cpf já cadastrado', Response::HTTP_CONFLICT);
-        }
-
-        $user = User::create($data);
-
-        return $user;
-
-
-    } catch (\Exception $exception) {
-        return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
-    }
-
     }
 }
---
+++ app/Mail/SendWelcomeUser.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class SendWelcomeUser extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $user;
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Obbrigado por se registrar',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            html: 'mails.welcomeTemplate',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
