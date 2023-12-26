921cb6c984cc2aced608310ee0f5bf8bfaaacb89
joseCnarciso <josecdia@hotmail.com>
2023-12-26 15:06:31 -0300

    concluido

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,6 @@ use App\Models\Plan;
 use App\Models\User;
 use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -17,24 +16,24 @@ class UserController extends Controller
 
     public function store(Request $request)
     {
-
         try {
 
-            $data = $request->all();
-
-            $request->validate([
+            $data = $request->validate([
                 'name' => 'required|string|max:255',
                 'email' => 'email|required|max:255',
                 'password' => 'string|required|min:8|max:32',
                 'plan_id' => 'required|integer',
                 'cpf' => 'string|required|min:14|max:14',
-                'date_birth' => 'date|required'
+                'date_birth' => 'date|required',
+
             ]);
 
             if (User::where('email', $data['email'])->exists()) {
+
                 return $this->error('Email já cadastrado', Response::HTTP_CONFLICT);
             }
             if (User::where('cpf', $data['cpf'])->exists()) {
+
                 return $this->error('cpf já cadastrado', Response::HTTP_CONFLICT);
             }
 
@@ -43,10 +42,10 @@ class UserController extends Controller
 
             Mail::to($user->email, $user->name)->send(new SendWelcomeUser($user, $plan));
 
-
-
             return $user;
+
         } catch (\Exception $exception) {
+
             return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
         }
     }
