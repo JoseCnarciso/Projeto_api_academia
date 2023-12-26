4be0da02c02371f3c2e4adbf7b78f77ff1d2ddc7
joseCnarciso <josecdia@hotmail.com>
2023-12-26 15:09:00 -0300

    concluido

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,33 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ExerciseController;
+use App\Http\Controllers\StudentsController;
+use App\Http\Controllers\ExportStudentPDFController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WorkoutController;
+use App\Http\Middleware\ValidateLimitStudentsToUser;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
-    // rotas privadas
+
+    Route::post('students', [StudentsController::class, 'store'])->middleware(ValidateLimitStudentsToUser::class);
+
+    Route::get('students/export-Student-PDF', [ExportStudentPDFController::class, 'showPerfilStudentPdf']);
+
+    Route::get('students', [StudentsController::class, 'index']);
+    Route::put('students/{id}', [StudentsController::class, 'update']);
+    Route::delete('students/{id}', [StudentsController::class, 'destroy']);
+    Route::get('students/{id}', [StudentsController::class, 'show']);
+    Route::get('/students/{id}/workouts', [StudentsController::class, 'showWorkoutsStudents']);
+    Route::post('workouts', [WorkoutController::class, 'store']);
+
+
     Route::get('exercises', [ExerciseController::class, 'index']);
     Route::post('exercises', [ExerciseController::class, 'store']);
     Route::delete('exercises/{id}', [ExerciseController::class, 'destroy']);
 
-    Route::get('dashboard', [DashboardController::class, 'index']);
     Route::post('logout', [AuthController::class, 'logout']);
+    Route::get('dashboard', [DashboardController::class, 'index']);
 });
 
 Route::post('users', [UserController::class, 'store']);
